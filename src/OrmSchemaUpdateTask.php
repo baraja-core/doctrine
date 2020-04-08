c11862a825a3e58ed5303bdbbc598782ac339e4b
Jan Barášek <janbarasek@gmail.com>
2020-04-08 12:49:09 +0200

    Task: Native internal call OrmSchemaUpdate for fully works use

--- src/OrmSchemaUpdateTask.php
+++ src/OrmSchemaUpdateTask.php
@@ -7,9 +7,11 @@ namespace Baraja\Doctrine;
 
 use Baraja\PackageManager\Composer\BaseTask;
 use Baraja\PackageManager\Helpers;
+use Contributte\Console\Application;
+use Symfony\Component\Console\Input\ArgvInput;
 
 /**
- * Priority: 5
+ * Priority: 99
  */
 final class OrmSchemaUpdateTask extends BaseTask
 {
@@ -19,35 +21,18 @@ final class OrmSchemaUpdateTask extends BaseTask
 	 */
 	public function run(): bool
 	{
-		echo 'BasePath: ' . ($basePath = \dirname(__DIR__, 4) . '/') . "\n\n";
+		echo 'Using PHP version: ' . PHP_VERSION . "\n\n";
 
-		if (Helpers::functionIsAvailable('shell_exec')) {
-			if (preg_match('/^PHP\s*((\d+)\.\d+)/', str_replace("\n", ' ', shell_exec('php -v')), $phpVersionParser)) {
-				$command = ($phpVersionParser[2] === '5' ? 'php7.1' : 'php')
-					. ' ' . rtrim(str_replace('\\', '/', $basePath), '/') . '/www/index.php o:s:u -f --dump-sql';
+		try {
+			/** @var Application $app */
+			$app = $this->getContainer()->getByType(Application::class);
+			$app->setAutoExit(false);
+			$app->run(new ArgvInput(['index.php', 'orm:schema-tool:update', '-f', '--dump-sql']));
+		} catch (\Throwable $e) {
+			Helpers::terminalRenderError($e->getMessage());
+			Helpers::terminalRenderCode($e->getFile(), $e->getLine());
 
-				echo 'Using PHP ' . $phpVersionParser[1] . "\n";
-				echo 'Command: ' . $command;
-				echo "\n\n";
-
-				if (preg_match('/^ERROR:/', $output = trim($this->liveExecuteCommand($command)))) {
-					Helpers::terminalRenderError($output);
-					if (\is_file($logPath = $basePath . '/log/exception.log') === true) {
-						$logLine = trim((string) ($data[\count($data = file($logPath)) - 1] ?? '???'));
-
-						if (preg_match('/(exception--[\d-]+--[a-f\d]+\.html)/', $logLine, $logLineParser)) {
-							Helpers::terminalRenderError('Logged to file: ' . $logLineParser[1]);
-						}
-
-						Helpers::terminalRenderError($logLine);
-					}
-				}
-			}
-		} else {
-			Helpers::terminalRenderError(
-				'Function shell_exec() is not available here.' . "\n\n"
-				. 'Please run command "php www/index.php o:s:u -f --dump-sql" manually.'
-			);
+			return false;
 		}
 
 		return true;
@@ -59,36 +44,6 @@ final class OrmSchemaUpdateTask extends BaseTask
 	 */
 	public function getName(): string
 	{
-		return 'o:s:u -f --dump-sql';
-	}
-
-
-	/**
-	 * Execute the given command by displaying console output live to the user.
-	 *
-	 * @param string $command
-	 * @return string
-	 */
-	private function liveExecuteCommand(string $command): string
-	{
-		while (@ob_end_flush()) {
-			continue;
-		}
-
-		$proc = popen($command . ' 2>&1 ; echo Exit status : $?', 'r');
-		$output = '';
-
-		while (feof($proc) === false) {
-			$liveOutput = fread($proc, 4096);
-			$output .= $liveOutput;
-			echo (string) $liveOutput;
-			@flush();
-		}
-
-		pclose($proc);
-
-		return preg_match('/\d+$/', $output, $matches)
-			? str_replace('Exit status: ' . ($matches[0] ?? ''), '', $output)
-			: '';
+		return 'Update Doctrine database schema';
 	}
 }
