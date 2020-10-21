f09f19fc6d4f355502386c54ad4f1bcf02c21cd4
Jan Barášek <janbarasek@gmail.com>
2020-10-21 14:42:24 +0200

    IdentifierUnsigned: Add integer identifier with best range and performance.

---
+++ src/Identifier/IdentifierUnsigned.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Baraja\Doctrine\Identifier;
+
+
+use Doctrine\ORM\Mapping as ORM;
+
+trait IdentifierUnsigned
+{
+
+	/**
+	 * @var int|null
+	 * @ORM\Id
+	 * @ORM\Column(type="integer", unique=true, options={"unsigned"=true})
+	 * @ORM\GeneratedValue
+	 */
+	protected $id;
+
+
+	public function getId(): ?int
+	{
+		if ($this->id === null) {
+			throw new \RuntimeException('Entity ID does not exist yet. Did you call ->persist() method first?');
+		}
+
+		return (int) $this->id;
+	}
+
+
+	public function setId(?int $id = null): void
+	{
+		throw new \LogicException('Can not set identifier "' . $id . '", please use trait UuidIdentifier.');
+	}
+
+
+	public function dangerouslySetLegacyId(int $id): void
+	{
+		$this->id = $id;
+		throw new \RuntimeException('The ID was passed unsafely. Please catch this exception if it was intended.');
+	}
+
+
+	public function __clone()
+	{
+		$this->id = null;
+	}
+}
