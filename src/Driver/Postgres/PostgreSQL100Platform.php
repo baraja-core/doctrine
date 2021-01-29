7bda8776db60035f1712dea99b9662684e875947
Jan Barášek <janbarasek@gmail.com>
2021-01-29 15:09:50 +0100

    Fix Nette Codestyle

--- src/Driver/Postgres/PostgreSQL100Platform.php
+++ src/Driver/Postgres/PostgreSQL100Platform.php
@@ -12,14 +12,18 @@ class PostgreSQL100Platform extends PostgreSQL94Platform
 {
 	public function getListSequencesSQL($database): string
 	{
-		return 'SELECT sequence_name AS relname,
-                       sequence_schema AS schemaname,
-                       minimum_value AS min_value,
-                       increment AS increment_by
-                FROM   information_schema.sequences
-                WHERE  sequence_catalog = ' . $this->quoteStringLiteral($database) . '
-                AND    sequence_schema NOT LIKE \'pg\_%\'
-                AND    sequence_schema != \'information_schema\'';
+		return (string) str_replace('%var%', $this->quoteStringLiteral($database),
+			<<<SQL
+SELECT sequence_name AS relname,
+	   sequence_schema AS schemaname,
+	   minimum_value AS min_value,
+	   increment AS increment_by
+FROM   information_schema.sequences
+WHERE  sequence_catalog = %var%
+AND    sequence_schema NOT LIKE 'pg\_%'
+AND    sequence_schema != 'information_schema'
+SQL
+		);
 	}
 
 
