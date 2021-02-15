5434e89aba940a3944f226b08958dbd950a5b851
Jan Barášek <janbarasek@gmail.com>
2021-02-15 22:10:30 +0100

    Sqlite3Cache: Fix random exceptions like "database is locked".

--- src/Cache/SQLite3Cache.php
+++ src/Cache/SQLite3Cache.php
@@ -7,14 +7,26 @@ namespace Baraja\Doctrine\Cache;
 
 final class SQLite3Cache extends \Doctrine\Common\Cache\SQLite3Cache
 {
-	public function __construct(string $cachePath)
+	public function __construct(string $cachePath, int $ttl = 25)
 	{
 		$cache = new \SQLite3($cachePath);
 		$cache->enableExceptions(true);
 		if ($cache->busyTimeout(60_000) === false) {
 			throw new \RuntimeException('SQLite3 cache: Can not set busy timeout: ' . $cache->lastErrorMsg(), $cache->lastErrorCode());
 		}
-		$cache->exec('PRAGMA journal_mode = wal;');
+		do {
+			try {
+				$cache->exec('PRAGMA journal_mode = wal;');
+				break;
+			} catch (\Throwable $e) {
+				if ($ttl > 0 && str_contains($e->getMessage(), 'database is locked')) {
+					usleep(100_000); // 100 ms
+					$ttl--;
+				} else {
+					throw new \RuntimeException('TTL expired: ' . $e->getMessage(), $e->getCode(), $e);
+				}
+			}
+		} while (true);
 
 		parent::__construct($cache, 'doctrine');
 	}
