06c8b46441d6331cf7de1eaea25ca0f6fe0b71f2
Jan Barášek <jan.barasek@packeta.com>
2021-06-08 16:04:14 +0200

    Event: Init logger entity.

---
+++ src/Logger/Event.php
@@ -0,0 +1,111 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Baraja\Doctrine\Logger;
+
+
+final class Event
+{
+	private float $start;
+
+	private ?float $end = null;
+
+	private ?float $duration = null;
+
+
+	/**
+	 * @param mixed[] $params
+	 * @param mixed[] $types
+	 * @param array{file: string, line: int, snippet: string}|null $location
+	 */
+	public function __construct(
+		private string $sql,
+		private string $hash,
+		private array $params,
+		private array $types,
+		private float $delayTime,
+		private ?array $location,
+	) {
+		$this->start = (float) microtime(true);
+	}
+
+
+	public function end(): void
+	{
+		$end = (float) microtime(true);
+		$this->end = $end;
+		$this->duration = $end - $this->start;
+	}
+
+
+	public function getSql(): string
+	{
+		return $this->sql;
+	}
+
+
+	public function getHash(): string
+	{
+		return $this->hash;
+	}
+
+
+	/**
+	 * @return mixed[]
+	 */
+	public function getParams(): array
+	{
+		return $this->params;
+	}
+
+
+	/**
+	 * @return mixed[]
+	 */
+	public function getTypes(): array
+	{
+		return $this->types;
+	}
+
+
+	public function getDelayTime(): float
+	{
+		return $this->delayTime;
+	}
+
+
+	/**
+	 * @return array{file: string, line: int, snippet: string}|null
+	 */
+	public function getLocation(): ?array
+	{
+		return $this->location;
+	}
+
+
+	public function getStart(): float
+	{
+		return $this->start;
+	}
+
+
+	public function getEnd(): ?float
+	{
+		return $this->end;
+	}
+
+
+	public function getDuration(): ?float
+	{
+		return $this->duration;
+	}
+
+
+	public function getDurationMs(): ?float
+	{
+		return $this->duration
+			? $this->duration * 1_000
+			: null;
+	}
+}
