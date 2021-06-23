b76c7c4db0793acaecc41a14e20f1b4cb1c9334c
Jan Barášek <janbarasek@gmail.com>
2021-06-23 11:58:12 +0200

    Cache: Use native implementation for legacy cache services.

---
+++ src/Cache/FilesystemCache.php
@@ -0,0 +1,96 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Baraja\Doctrine\Cache;
+
+
+final class FilesystemCache extends FileCache
+{
+	public const EXTENSION = '.doctrinecache.data';
+
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function __construct($directory, $extension = self::EXTENSION, $umask = 0002)
+	{
+		parent::__construct($directory, $extension, $umask);
+	}
+
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function doFetch($id)
+	{
+		$data = '';
+		$lifetime = -1;
+		$filename = $this->getFilename($id);
+
+		if (!is_file($filename)) {
+			return false;
+		}
+
+		$resource = fopen($filename, 'rb');
+		$line = fgets($resource);
+
+		if ($line !== false) {
+			$lifetime = (int) $line;
+		}
+		if ($lifetime !== 0 && $lifetime < time()) {
+			fclose($resource);
+
+			return false;
+		}
+
+		while (($line = fgets($resource)) !== false) {
+			$data .= $line;
+		}
+
+		fclose($resource);
+
+		return unserialize($data);
+	}
+
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function doContains($id)
+	{
+		$lifetime = -1;
+		$filename = $this->getFilename($id);
+
+		if (!is_file($filename)) {
+			return false;
+		}
+
+		$resource = fopen($filename, 'rb');
+		$line = fgets($resource);
+
+		if ($line !== false) {
+			$lifetime = (int) $line;
+		}
+
+		fclose($resource);
+
+		return $lifetime === 0 || $lifetime > time();
+	}
+
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function doSave($id, $data, $lifeTime = 0)
+	{
+		if ($lifeTime > 0) {
+			$lifeTime = time() + $lifeTime;
+		}
+
+		$data = serialize($data);
+		$filename = $this->getFilename($id);
+
+		return $this->writeFile($filename, $lifeTime . PHP_EOL . $data);
+	}
+}
--- src/Cache/SQLite3Cache.php
+++ src/Cache/SQLite3Cache.php
@@ -5,9 +5,35 @@ declare(strict_types=1);
 namespace Baraja\Doctrine\Cache;
 
 
-final class SQLite3Cache extends \Doctrine\Common\Cache\SQLite3Cache
+use Doctrine\Common\Cache\CacheProvider;
+use function array_search;
+use function implode;
+use function serialize;
+use function sprintf;
+use function time;
+use function unserialize;
+
+final class SQLite3Cache extends CacheProvider
 {
-	public function __construct(string $cachePath, int $ttl = 25)
+	/** The ID field will store the cache key. */
+	public const ID_FIELD = 'k';
+
+	/** The data field will store the serialized PHP value. */
+	public const DATA_FIELD = 'd';
+
+	/** The expiration field will store a date value indicating when the cache entry should expire. */
+	public const EXPIRATION_FIELD = 'e';
+
+	private \SQLite3 $sqlite;
+
+	private string $table;
+
+
+	/**
+	 * Calling the constructor will ensure that the database file and table
+	 * exist and will create both if they don't.
+	 */
+	public function __construct(string $cachePath, int $ttl = 25, ?string $table = null)
 	{
 		$cache = new \SQLite3($cachePath);
 		$cache->enableExceptions(true);
@@ -31,6 +57,170 @@ final class SQLite3Cache extends \Doctrine\Common\Cache\SQLite3Cache
 			}
 		} while (true);
 
-		parent::__construct($cache, 'doctrine');
+		$this->sqlite = $cache;
+		$this->table = $table ?? 'doctrine';
+		$this->ensureTableExists();
+	}
+
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function doFetch($id)
+	{
+		$item = $this->findById($id);
+		if (!$item) {
+			return false;
+		}
+
+		return unserialize($item[self::DATA_FIELD]);
+	}
+
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function doContains($id)
+	{
+		return $this->findById($id, false) !== null;
+	}
+
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function doSave($id, $data, $lifeTime = 0)
+	{
+		$statement = $this->sqlite->prepare(
+			sprintf(
+				'INSERT OR REPLACE INTO %s (%s) VALUES (:id, :data, :expire)',
+				$this->table,
+				implode(',', $this->getFields())
+			)
+		);
+
+		$statement->bindValue(':id', $id);
+		$statement->bindValue(':data', serialize($data), SQLITE3_BLOB);
+		$statement->bindValue(':expire', $lifeTime > 0 ? time() + $lifeTime : null);
+
+		return $statement->execute() instanceof SQLite3Result;
+	}
+
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function doDelete($id)
+	{
+		[$idField] = $this->getFields();
+
+		$statement = $this->sqlite->prepare(
+			sprintf(
+				'DELETE FROM %s WHERE %s = :id',
+				$this->table,
+				$idField
+			)
+		);
+
+		$statement->bindValue(':id', $id);
+
+		return $statement->execute() instanceof SQLite3Result;
+	}
+
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function doFlush()
+	{
+		return $this->sqlite->exec(sprintf('DELETE FROM %s', $this->table));
+	}
+
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function doGetStats()
+	{
+		// no-op.
+	}
+
+
+	private function ensureTableExists(): void
+	{
+		$this->sqlite->exec(
+			sprintf(
+				'CREATE TABLE IF NOT EXISTS %s(%s TEXT PRIMARY KEY NOT NULL, %s BLOB, %s INTEGER)',
+				$this->table,
+				static::ID_FIELD,
+				static::DATA_FIELD,
+				static::EXPIRATION_FIELD
+			)
+		);
+	}
+
+
+	/**
+	 * Find a single row by ID.
+	 *
+	 * @return mixed[]|null
+	 */
+	private function findById(mixed $id, bool $includeData = true): ?array
+	{
+		[$idField] = $fields = $this->getFields();
+
+		if (!$includeData) {
+			$key = array_search(static::DATA_FIELD, $fields);
+			unset($fields[$key]);
+		}
+
+		$statement = $this->sqlite->prepare(
+			sprintf(
+				'SELECT %s FROM %s WHERE %s = :id LIMIT 1',
+				implode(',', $fields),
+				$this->table,
+				$idField
+			)
+		);
+
+		$statement->bindValue(':id', $id, SQLITE3_TEXT);
+
+		$item = $statement->execute()
+			->fetchArray(SQLITE3_ASSOC);
+
+		if ($item === false) {
+			return null;
+		}
+		if ($this->isExpired($item)) {
+			$this->doDelete($id);
+
+			return null;
+		}
+
+		return $item;
+	}
+
+
+	/**
+	 * Gets an array of the fields in our table.
+	 *
+	 * @phpstan-return array{string, string, string}
+	 */
+	private function getFields(): array
+	{
+		return [self::ID_FIELD, self::DATA_FIELD, self::EXPIRATION_FIELD];
+	}
+
+
+	/**
+	 * Check if the item is expired.
+	 *
+	 * @param mixed[] $item
+	 */
+	private function isExpired(array $item): bool
+	{
+		return isset($item[self::EXPIRATION_FIELD])
+			&& $item[self::EXPIRATION_FIELD] !== null
+			&& $item[self::EXPIRATION_FIELD] < time();
 	}
 }
