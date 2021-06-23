06a7f52957aaa1a410ba59424b56671a26d2b5fc
Jan Barášek <janbarasek@gmail.com>
2021-06-23 11:27:30 +0200

    Remove deprecated logic.

--- src/Exception/DatabaseException.php
+++ src/Exception/DatabaseException.php
@@ -7,32 +7,4 @@ namespace Baraja\Doctrine;
 
 class DatabaseException extends \RuntimeException
 {
-	/**
-	 * @deprecated since 2021-06-01 method will be removed in next master version.
-	 * @throws DatabaseException
-	 */
-	public static function e(\Throwable $e): void
-	{
-		throw new self($e->getMessage(), $e->getCode(), $e);
-	}
-
-
-	/**
-	 * @deprecated since 2021-06-01 method will be removed in next master version.
-	 * @throws DatabaseException
-	 */
-	public static function canNotSetIdentifier(?string $id): void
-	{
-		throw new self('Can not set identifier "' . $id . '", please use trait UuidIdentifier.');
-	}
-
-
-	/**
-	 * @deprecated since 2021-06-01 method will be removed in next master version.
-	 * @throws DatabaseException
-	 */
-	public static function remapDifferentTypes(string $fromTable, string $to): void
-	{
-		throw new self('Entities for remap must be same table type, "' . $fromTable . '" and "' . $to . '" given.');
-	}
 }
