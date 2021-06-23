e9f56e88c80ae9eace5af5794273cece6923fd78
Jan Barášek <janbarasek@gmail.com>
2021-06-23 11:58:12 +0200

    Cache: Use native implementation for legacy cache services.

---
+++ src/Cache/ApcuCache.php
@@ -0,0 +1,106 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Baraja\Doctrine\Cache;
+
+
+use Doctrine\Common\Cache\Cache;
+use Doctrine\Common\Cache\CacheProvider;
+
+class ApcuCache extends CacheProvider
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function doFetch($id)
+	{
+		return apcu_fetch($id);
+	}
+
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function doContains($id)
+	{
+		return apcu_exists($id);
+	}
+
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function doSave($id, $data, $lifeTime = 0)
+	{
+		return apcu_store($id, $data, $lifeTime);
+	}
+
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function doDelete($id)
+	{
+		// apcu_delete returns false if the id does not exist
+		return apcu_delete($id) || !apcu_exists($id);
+	}
+
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function doDeleteMultiple(array $keys)
+	{
+		$result = apcu_delete($keys);
+
+		return $result !== false && count($result) !== count($keys);
+	}
+
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function doFlush()
+	{
+		return apcu_clear_cache();
+	}
+
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function doFetchMultiple(array $keys)
+	{
+		return apcu_fetch($keys) ?: [];
+	}
+
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function doSaveMultiple(array $keysAndValues, $lifetime = 0)
+	{
+		$result = apcu_store($keysAndValues, null, $lifetime);
+
+		return empty($result);
+	}
+
+
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function doGetStats()
+	{
+		$info = apcu_cache_info(true);
+		$sma = apcu_sma_info();
+
+		return [
+			Cache::STATS_HITS => $info['num_hits'],
+			Cache::STATS_MISSES => $info['num_misses'],
+			Cache::STATS_UPTIME => $info['start_time'],
+			Cache::STATS_MEMORY_USAGE => $info['mem_size'],
+			Cache::STATS_MEMORY_AVAILABLE => $sma['avail_mem'],
+		];
+	}
+}
