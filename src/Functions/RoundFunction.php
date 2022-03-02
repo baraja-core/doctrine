93c64d7930ba1ccdbf8e2d7550b67b7f9ebc84f3
Jan Barášek <janbarasek@gmail.com>
2022-03-02 14:24:34 +0100

    Function ROUND: Fix compatibility with Doctrine Parser.

--- src/Functions/RoundFunction.php
+++ src/Functions/RoundFunction.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace Baraja\Doctrine;
 
 
+use Doctrine\ORM\Query\AST\ArithmeticTerm;
 use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\AST\Node;
 use Doctrine\ORM\Query\AST\SimpleArithmeticExpression;
 use Doctrine\ORM\Query\Lexer;
 use Doctrine\ORM\Query\Parser;
@@ -13,22 +15,42 @@ use Doctrine\ORM\Query\SqlWalker;
 
 final class RoundFunction extends FunctionNode
 {
-	private SimpleArithmeticExpression $arithmeticExpression;
+	private SimpleArithmeticExpression|ArithmeticTerm|null $firstExpression = null;
 
-
-	public function getSql(SqlWalker $sqlWalker): string
-	{
-		return 'ROUND(' . $sqlWalker->walkSimpleArithmeticExpression($this->arithmeticExpression) . ')';
-	}
+	private Node|string|null $secondExpression = null;
 
 
 	public function parse(Parser $parser): void
 	{
+		$lexer = $parser->getLexer();
 		$parser->match(Lexer::T_IDENTIFIER);
 		$parser->match(Lexer::T_OPEN_PARENTHESIS);
 
-		$this->arithmeticExpression = $parser->SimpleArithmeticExpression();
+		/** @var SimpleArithmeticExpression|ArithmeticTerm $firstExpression */
+		$firstExpression = $parser->SimpleArithmeticExpression();
+		$this->firstExpression = $firstExpression;
+
+		// parse second parameter if available
+		if (Lexer::T_COMMA === $lexer->lookahead['type']) {
+			$parser->match(Lexer::T_COMMA);
+			$this->secondExpression = $parser->ArithmeticPrimary();
+		}
 
 		$parser->match(Lexer::T_CLOSE_PARENTHESIS);
+	}
+
+
+	public function getSql(SqlWalker $sqlWalker): string
+	{
+		// use second parameter if parsed
+		if ($this->secondExpression !== null) {
+			return 'ROUND('
+				. $this->firstExpression->dispatch($sqlWalker)
+				. ', '
+				. $this->secondExpression->dispatch($sqlWalker)
+				. ')';
+		}
+
+		return 'ROUND(' . $this->firstExpression->dispatch($sqlWalker) . ')';
 	}
 }
